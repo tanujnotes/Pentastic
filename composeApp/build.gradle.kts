a227e2d9c80c55c087c22b6b67a16738c690d0b3
Tanuj <thetanuj@hotmail.com>
2026-01-01 15:54:07 +0530

    App version bumped to 1 (v0.1)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -99,7 +99,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "v0.1"
     }
     packaging {
         resources {
