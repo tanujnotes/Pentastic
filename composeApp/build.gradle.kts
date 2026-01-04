334601c894d1c5b82966fd6f66d7ad44b325dd1b
Tanuj <thetanuj@hotmail.com>
2026-01-04 20:22:13 +0530

    App version bumped to 2 (v0.2)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -92,8 +92,8 @@ android {
         applicationId = "app.pentastic"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "v0.1"
+        versionCode = 2
+        versionName = "v0.2"
     }
     packaging {
         resources {
