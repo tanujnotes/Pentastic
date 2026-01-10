71cd80e99e15ab4179689a1dcd83c43f531bc09a
Tanuj <thetanuj@hotmail.com>
2026-01-10 22:47:13 +0530

    App version bumped to 5 (v0.5)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -92,8 +92,8 @@ android {
         applicationId = "app.pentastic"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "v0.4"
+        versionCode = 5
+        versionName = "v0.5"
     }
     packaging {
         resources {
